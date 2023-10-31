5f75d89eeb42334f19daa9533aacbaf5dcedac68
Brandon Aheimer <bcaheimer@email.wm.edu>
2023-10-31 06:56:38 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+**Requirements**
+
+The tutorial file uses the Pillow and requests libraries, which can be installed with the following commands.
+
+`python -m pip install Pillow`
+
+`python -m pip install requests`
+
+**Create a Hugging Face account**
+
+Unfortunately OpenAI has no free plans available for their API, but there are many free models available with similar capabilities. We will be using a couple of these free alternatives in this tutorial.
+1. Go to https://huggingface.co/ and click sign up on the top right corner.
+2. Once signed in, click your profile picture, go to settings, and select Access Tokens from the menu on the left
+3. Create a new token, give it a name, and give it “read” access
+
+**Tutorial**
+
+The skeleton file already has a method implemented that generates an image of a given animal wearing a funny hat using the stable-diffusion-xl-base-1.0 model from stabilityai. Your job is to complete the implementation of the generate_caption and main functions to paste a fun fact on top of these animal pictures.
+
+**Submission**
+
+Email Alejandro the image and caption you get for a flamingo so he can verify you completed the tutorial.
---
+++ tutorial.py
@@ -0,0 +1,63 @@
+import requests
+from PIL import Image, ImageDraw 
+import io
+
+
+image_API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
+caption_API_URL = "https://api-inference.huggingface.co/models/HuggingFaceH4/zephyr-7b-beta"
+# TODO put in access token
+headers = {"Authorization": "Bearer ACCESS_TOKEN"}
+
+
+""" Generates image base on text prompt
+	Parameters
+    ----------
+	prompt | str
+		text feed for image generation
+
+	Returns
+    -------
+    bytes
+		bytes corresponding to generated image
+"""
+def generate_image(prompt):
+	payload = {"inputs": prompt,}
+	response = requests.post(image_API_URL, headers=headers, json=payload)
+	return response.content
+
+""" Generates caption based on image
+	Parameters
+    ----------
+	prompt | bytes
+		bytes corresponding to image
+
+	Returns
+    -------
+    str
+		text describing image
+"""
+def generate_caption(prompt):
+	# TODO implement function
+	# hint: should use json() to get dictionary from response
+	pass
+
+def main():
+	while True:
+		animal = input("Please input an animal, or \"Quit\": ")
+		if animal == "Quit":
+			break
+		try:
+			image_bytes = generate_image(animal+" wearing a funny hat")
+			image = Image.open(io.BytesIO(image_bytes))
+			image.show()
+
+			# TODO create a fun_fact about the animal using the text generation model
+			# draw = ImageDraw.Draw(image)
+			# draw.text((0, 0), fun_fact,(255,255,255))
+			# image.show()
+			
+		except Exception as error:
+			print("error: ", error)
+
+if __name__ == "__main__":
+	main()
